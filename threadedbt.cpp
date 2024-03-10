625d5332233e23a89208617648949c3e0cdd682b
Ahad-23 <ahad.shaikkh@gmail.com>
2024-03-10 19:30:53 +0530

    minor changes

--- threadedbt.cpp
+++ threadedbt.cpp
@@ -1,76 +1,100 @@
 #include <iostream>
 using namespace std;
-
-class tbtnode
-{
+class tbtnode {
     char data;
     bool rbit;
     bool lbit;
-    tbtnode *rightc;
     tbtnode *leftc;
-    friend class tbt;
-
-public:
-    tbtnode()
-    {
-        lbit = 0;
-        rbit = 0;
+    tbtnode *rightc;
+    tbtnode(){
+        lbit = rbit = false;
     }
+    friend class tbt;
 };
-class tbt{
+class tbt {
     tbtnode *head;
     public:
-    void create()
-    {
-    tbtnode *root=new tbtnode;
-    cout<<"Enter data for root node"<<endl;
-    cin>>root->data;
-    head->lbit=1;
-    root->leftc=root->rightc=head;
-    head->leftc=root;
-    char ch;
-    char ch1;
+    tbt() {
+        head = new tbtnode();
+        head->rbit = head->lbit = true;
+        head->leftc = head->rightc = head;
+    }
+    void create(){
+        tbtnode *root = new tbtnode();
+        cout << "Enter data of root node";
+        cin >> root->data;
+        head->lbit = 1;
+        head->leftc = root;
+        root->rightc = head;
+        root->leftc = head;
+        char ch1;
 
-    do{
-        int flag=0;
-        tbtnode *temp=root;
-        tbtnode *curr=new tbtnode;
-        cout<<"Enter data"<<endl;
-        cin>>curr->data;
-        while(flag==0){
-            cout<<" Left or Right (l/r)"<<endl;
-            cin>>ch;
-            if(ch=='l'){
-                if(temp->lbit==0){
-                    curr->rightc=temp;
-                    curr->leftc=temp->leftc;
-                    temp->leftc=curr;
-                    temp->lbit=1;
-                    flag=1;
+        do {
+            tbtnode *temp = root;
+            tbtnode *newnode = new tbtnode();
+            bool flag = false;
+
+            while (flag == false)
+            {
+                char ch;
+                cout << "Do you want to enter data to left or to right (l/r)";
+                cin >> ch;
+                cout << "Enter data";
+                cin >> newnode->data;
+                if (ch == 'l')
+                {
+                    if (temp->lbit == 0)
+                    {
+                        newnode->rightc = temp;
+                        newnode->leftc = temp->leftc;
+                        temp->leftc = newnode;
+                        temp->lbit = 1;
+                        flag = true;
+                    }
+                    else
+                    {
+                        temp = temp->leftc;
+                    }
                 }
-                else{
-                    temp=temp->leftc;
+                if (ch == 'r'){
+                    if (temp->rbit == 0)
+                    {
+                        newnode->leftc = temp;
+                        newnode->rightc = temp->rightc;
+                        temp->rightc = newnode;
+                        temp->rbit = 1;
+                        flag = true;
+                    }
+                    else
+                    {
+                        temp = temp->rightc;
+                    }
                 }
-            }
-            else if(ch=='r'){
-                if(temp->rbit==0){
-                    curr->leftc=temp;
-                    curr->rightc=temp->rightc;
-                    temp->rightc=curr;
-                    temp->rbit=1;
-                    flag=1;
+                cout << "Do you want to enter more nodes (y/n)";
+                cin >> ch1;
                 }
-                else{
-                    temp=temp->rightc;
-                }
-            }
+            }while (ch1 == 'y');
+
+    }
+    void inorder(){
+        tbtnode *temp = head;
+        while(1){
+            temp = inordersucc(temp);
+            if(temp == head) break;
+            cout << temp->data;
         }
-        cout<<"Enter more nodes (y/n)"<<endl;
-        cin>>ch1;
-    }while(ch1=='y');
-}
+    }
 
-   void preorder(){
+    tbtnode* inordersucc(tbtnode* temp){
+        tbtnode *x = temp->rightc;
+        if (temp->rbit== 1){
+            while (x->lbit==1)
+            {
+                x = x->leftc;
+            }}
+        return x;   
+    }
+    void preorder(){
         tbtnode* temp = head->leftc;
         while(temp!= head){
             cout << temp->data;
@@ -83,48 +107,14 @@ class tbt{
                 temp = temp->rightc;
             }
             temp = temp->rightc;
-}}
-
-    tbtnode* presuccr(tbtnode *temp);
-    void inorder()
-    {
-    tbtnode *temp=head;
-    while(1){
-        temp=insuccr(temp);
-        if(temp==head)
-            break;
-        cout<<temp->data;
-    }
-}
-    tbtnode *insuccr(tbtnode *temp){
-    tbtnode *x= new tbtnode;
-    x=temp->rightc;
-    if(temp->rbit==1){
-        while(x->lbit==1){
-            x=x->leftc;
         }
     }
-    return x;
-}
-    tbt(){
-        head=new tbtnode;
-        head->rbit=1;
-        head->leftc=head;
-        head->rightc=head;
-    }
-    friend class tbtnode;
 };
-
-
-
-
-
-
 
 int main(){
     tbt t;
-t.create();
-t.inorder();
-cout<<endl;
-t.preorder();
+    t.create();
+    t.inorder();
+    t.preorder();
+
 }
